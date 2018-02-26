d47cd498d9b5678f4f834b36ae96e734a5366ef1
Matthew Miller <matthew@mi11er.net>
2018-02-26 13:09:06 -0500

    feat(security): Change eslint-plugin-security rules to error

--- rules/security.js
+++ rules/security.js
@@ -6,20 +6,23 @@ const env = {
 const plugins = ['security', 'scanjs-rules', 'no-unsafe-innerhtml'];
 
 const rules = {
-  /* security rules */
-  'security/detect-buffer-noassert': 'warn',
-  'security/detect-child-process': 'warn',
-  'security/detect-disable-mustache-escape': 'warn',
-  'security/detect-eval-with-expression': 'warn',
-  'security/detect-new-buffer': 'warn',
-  'security/detect-no-csrf-before-method-override': 'warn',
-  'security/detect-non-literal-fs-filename': 'warn',
-  'security/detect-non-literal-regexp': 'warn',
-  'security/detect-non-literal-require': 'warn',
-  'security/detect-object-injection': 'warn',
-  'security/detect-possible-timing-attacks': 'warn',
-  'security/detect-pseudoRandomBytes': 'warn',
-  'security/detect-unsafe-regex': 'warn',
+  /**
+   *  security rules
+   *  https://github.com/nodesecurity/eslint-plugin-security
+   */
+  'security/detect-buffer-noassert': 'error',
+  'security/detect-child-process': 'error',
+  'security/detect-disable-mustache-escape': 'error',
+  'security/detect-eval-with-expression': 'error',
+  'security/detect-new-buffer': 'error',
+  'security/detect-no-csrf-before-method-override': 'error',
+  'security/detect-non-literal-fs-filename': 'error',
+  'security/detect-non-literal-regexp': 'error',
+  'security/detect-non-literal-require': 'error',
+  'security/detect-object-injection': 'error',
+  'security/detect-possible-timing-attacks': 'error',
+  'security/detect-pseudoRandomBytes': 'error',
+  'security/detect-unsafe-regex': 'error',
 
   /* no-unsafe-innerhtml rule */
   'no-unsafe-innerhtml/no-unsafe-innerhtml': 'error',
